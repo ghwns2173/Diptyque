605e11c9fdb23967ec4b53cf6aa1b1e507646e92
ghwns2173 <ghwjd2173@naver.com>
2024-11-08 17:14:59 +0900

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () { 
+document.addEventListener("DOMContentLoaded", function () {
   AOS.init();
-  
-  
+
+
   // top버튼
   // 1.변수선언하기
   const topBtn = document.querySelector(`.top_btn`);
@@ -33,8 +33,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const submenuTab = document.querySelectorAll(`.gnb li`);
   const submenuBox = document.querySelector(`.sub_menu_box`);
 
-  for (const li of submenuTab){
-    li.addEventListener(`mouseenter`, function(){
+  for (const li of submenuTab) {
+    li.addEventListener(`mouseenter`, function () {
       submenuBox.classList.add(`active`);
 
       // 탭메뉴 연결
@@ -42,9 +42,9 @@ document.addEventListener("DOMContentLoaded", function () {
       const subMenu = document.querySelectorAll(`.sub_menu`);
 
       // 전체적으로 서브메뉴 먼저 제거
-      for (const tabContent of subMenu){
+      for (const tabContent of subMenu) {
         tabContent.classList.remove(`active`);
-    }
+      }
 
       // data-tab 에 작성된 데이터명과 동일한 아이디명을 가진 서브메뉴는 출력 
       // const changeTab = document.getElementById(tab);
@@ -52,45 +52,82 @@ document.addEventListener("DOMContentLoaded", function () {
       changeTab.classList.add(`active`);
     });
   }
-   // 서브메뉴박스에서 마우스 나가면 기존 상태로 변경
-  submenuBox.addEventListener(`mouseleave`, function () { 
+  // 서브메뉴박스에서 마우스 나가면 기존 상태로 변경
+  submenuBox.addEventListener(`mouseleave`, function () {
     this.classList.remove(`active`);
-});
+  });
 
   // --------------------------------------------------
   //  sec2번 tab
   const tabLi = document.querySelectorAll(`.tab_box li`);
 
   // 추가 제거 부분
-  for(const li of tabLi){
-    li.addEventListener(`click`, function(){
+  for (const li of tabLi) {
+    li.addEventListener(`click`, function () {
       this.classList.add(`tabOn`);
 
-      for(const siblings of tabLi){
-        if(siblings != this){
+      for (const siblings of tabLi) {
+        if (siblings != this) {
           siblings.classList.remove(`tabOn`);
         }
       }
-    // 탭 연결
-    // li 안에 담아놓은 data 를 변수에 담아주기
+      // 탭 연결
+      // li 안에 담아놓은 data 를 변수에 담아주기
       const tabData = this.getAttribute(`data-alt`);
-    // 시용할 변수 선언
+      // 시용할 변수 선언
       const menuList = document.querySelectorAll(`.swiper_wrap`);
-      
-      for (const tabContent of menuList){
+
+      for (const tabContent of menuList) {
         tabContent.classList.remove(`tabOn`);
       }
       const changeTab = document.getElementById(tabData);
       changeTab.classList.add(`tabOn`);
     });
-    
+
   }
 
-  // sec2번 슬라이드
-  var swiper = new Swiper(".listSwiper", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    
+
+  // // sec2번 슬라이드
+  // new Swiper(".listSwiper", {
+
+  // });
+
+  // 스와이퍼 변수선언
+  let swiper = undefined;
+
+  // 선언식 함수
+  function initSwiper() {
+    // 윈도우에 너비를 변수에 저장
+    const windowWidth = window.innerWidth;
+
+    // 스와이퍼가 윈도우 960이상이면 작동하고 미만이면 작동중지
+    if (windowWidth >= 960 && swiper == undefined) {
+      // 스와이퍼
+      swiper = new Swiper(".listSwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 4,
+        spaceBetween: 20,
+
+      });
+    } else if (windowWidth < 960 && swiper != undefined) {
+      swiper.destroy();
+      swiper = undefined;
+    }
+  }
+
+  // 함수 전역 호출
+  initSwiper();
+
+
+  // 윈도우 리사이즈 될때 스와이퍼 자동 반응설정
+  window.addEventListener(`resize`, () => {
+    initSwiper(); // 함수 지역호출
   });
+
+
+
 
 }); //end
